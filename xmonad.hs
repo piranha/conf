2a394fafaf1bfe0436a530c157a571126bee76d1
Alexander Solovyov <piranha@piranha.org.ua>
2007-11-18 14:56:35 +0200

    xmonad config upgrades

--- xmonad.hs
+++ xmonad.hs
@@ -3,7 +3,6 @@ import Graphics.X11
 import System.IO (hPutStrLn)
 import qualified Data.Map as M
 import Data.Bits ((.|.))
-import Data.Ratio ((%))
 -- XMonad
 import XMonad
 import XMonad.Layouts
@@ -16,18 +15,21 @@ import XMonad.Actions.SwapWorkspaces
 import XMonad.Actions.WmiiActions
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.Named
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Tabbed
 import XMonad.Layout.ToggleLayouts
 import XMonad.Layout.Accordion
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.Ssh
 import XMonad.Prompt.Window
 import XMonad.Prompt.XMonad
 
+statusBarCmd = "xmobar"
 
-statusBarCmd= "dzen2 -e '' -w 800 -ta l -fg \"#a8a3f7\" -bg \"#3f3c6d\" -fn \"-xos4-terminus-medium-r-normal--16-160-72-72-c-80-iso8859-1\""
-
+-- prompt config
 ownXPConfig :: XPConfig
 ownXPConfig = defaultXPConfig 
               { font              = "-*-terminus-*-*-*-*-14-*-*-*-*-*-*-*"
@@ -38,19 +40,23 @@ ownXPConfig = defaultXPConfig
               , borderColor       = "#FFFFFF"
               }
 
+-- tabbed layout config
 tabbedConf :: TConf
 tabbedConf = defaultTConf { fontName = "-xos4-terminus-medium-r-normal-*-16-*-*-*-*-*-*-*" }
 
+-- dynamic log config
 ownPP h = defaultPP
           { ppOutput = hPutStrLn h
---           , ppTitle  = (\x -> "")
+          , ppCurrent = xmobarColor "white" "" . wrap "[" "]"
+          , ppVisible = xmobarColor "yellow" "" . wrap "*" "*"
+          , ppTitle = xmobarColor "#00ee00" ""
           , ppSep    = " "
           , ppLayout = (\x -> "")
           }
 
+-- keys config
 ownKeys conf@(XConfig {modMask = modMask}) = M.fromList $
     [ ((modMask   ,              xK_f      ), sendMessage ToggleLayout)
-    , ((modMask .|. controlMask, xK_x      ), xmonadPrompt ownXPConfig)
     , ((modMask,                 xK_x      ), submap . M.fromList $
        [ ((0, xK_z), spawn "quodlibet --previous")
        , ((0, xK_x), spawn "quodlibet --play-pause")
@@ -58,7 +64,10 @@ ownKeys conf@(XConfig {modMask = modMask}) = M.fromList $
        , ((0, xK_v), spawn "quodlibet --next")
        , ((0, xK_a), spawn "quodlibet --toggle-window")
        ])
-    , ((modMask,                 xK_g      ), windowPromptGoto ownXPConfig)
+    , ((modMask .|. controlMask, xK_x      ), xmonadPrompt ownXPConfig)
+    , ((modMask,                 xK_F1     ), windowPromptGoto ownXPConfig)
+    , ((modMask,                 xK_F2     ), shellPrompt ownXPConfig)
+    , ((modMask,                 xK_F3     ), sshPrompt ownXPConfig)
     , ((modMask,                 xK_Insert ), spawn "import -window root ~/screenshot-`date +\"%F--%H-%M-%S\"`.png")
     , ((modMask,                 xK_Pause  ), spawn "gnome-screensaver-command --lock")
     , ((modMask,                 xK_Return ), dwmpromote)
@@ -71,8 +80,6 @@ ownKeys conf@(XConfig {modMask = modMask}) = M.fromList $
     , ((modMask .|. shiftMask,   xK_Up     ), withFocused (keysResizeWindow (0, -20) (0, 0)))
     , ((modMask .|. shiftMask,   xK_Down   ), withFocused (keysResizeWindow (0, 20) (0, 0)))
     , ((modMask,                 xK_BackSpace), focusUrgent)
-    -- currently unused because of already working keys :-)
-    -- , ((0,                    0x1008ff12), spawn "amixer -q set Master toggle")
     , ((modMask,                 xK_F12    ), spawn "amixer -q set PCM 2dB+")
     , ((modMask,                 xK_F11    ), spawn "amixer -q set PCM 2dB-")
     ]
@@ -80,26 +87,26 @@ ownKeys conf@(XConfig {modMask = modMask}) = M.fromList $
     [((modMask .|. controlMask, k), windows $ swapWithCurrent i)
          | (i, k) <- zip (workspaces conf) [xK_1 .. xK_9]]
 
-ownConfig h = defaultConfig
+-- overall config
+ownConfig statusbar = defaultConfig
                 { borderWidth        = 1
                 , normalBorderColor  = "#3f3c6d"
                 , focusedBorderColor = "#FF0000"
                 , terminal           = "urxvtc"
                 , defaultGaps        = [(18,0,0,0), (18,0,0,0)]
                 , modMask            = mod4Mask
-                , logHook            = dynamicLogWithPP $ ownPP h
-                , layoutHook         = Layout $ withUrgencyHook dzenUrgencyHook
+                , logHook            = dynamicLogWithPP $ ownPP statusbar
+                , layoutHook         = smartBorders
                                        $ toggleLayouts (noBorders Full)
-                                       $ smartBorders
-                                       $ tiled 
-                                       ||| Mirror tiled
-                                       ||| owntab
-                                       ||| Accordion
+                                       $ Named "vert" tiled
+                                       ||| Named "horiz" (Mirror tiled)
+                                       ||| Named "tabbed" owntab
+                                       ||| Named "accordion" Accordion
                 , keys               = \c -> ownKeys c `M.union` keys defaultConfig c
                 }
     where
       owntab = tabbed shrinkText tabbedConf
-      tiled = Tall 1 0.03 0.5
+      tiled = Tall 1 (3/100) (1/2)
 
 main = do statusbar <- spawnPipe statusBarCmd
           xmonad $ ownConfig statusbar
