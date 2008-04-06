823be3db20272a210e3841d45d0ce2f7975c48e3
Alexander Solovyov <piranha@piranha.org.ua>
2008-04-06 21:48:55 +0300

    custom faces for white background

--- .emacs.d/load/custom_init.el
+++ .emacs.d/load/custom_init.el
@@ -9,4 +9,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:foreground "dark green"))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background light)) (:foreground "SaddleBrown")))))
