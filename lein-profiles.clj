71b759ffaeee060598bbe0ada18adc34352f23af
Alexander Solovyov <alexander@solovyov.net>
2013-11-08 09:16:24 +0200

    debugging macro for console

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,15 +1,17 @@
 {:user {:plugins [[lein-shegon "0.1.1"]
                   [lein-ancient "0.4.4"]
                   [jonase/eastwood "0.0.2"]
+                  [lein-deps-tree "0.1.2"]
                   [lein-try "0.3.0"]
                   ;[lein-zeroconf "0.1.0"]
                   ;[lein-ritz "0.7.0"]
                   ]
-        :dependencies [[spyscope "0.1.3"]
+        :dependencies [;[spyscope "0.1.3"]
                        [debug-repl "0.3.2"]]
         :injections [;(require 'spyscope.core)
                      (require '[alex-and-georges.debug-repl :refer [debug-repl]])
                      (require '[clojure.string :as s])
+
                      (defn parse-qs [a]
                        (->> a
                             (#(s/split % #"&"))
@@ -18,9 +20,21 @@
                                    [(keyword k)
                                     (if v (java.net.URLDecoder/decode v))]))
                             (into {})))
+
                      (defn unparse-qs [qs]
                        (->> qs
                            (map #(vec [(name (first %))
                                        (java.net.URLEncoder/encode (str (second %)))]))
                            (map #(s/join "=" %))
-                           (s/join "&")))]}}
+                           (s/join "&")))
+
+                     (defmacro dbg [x]
+                       `(let [x# ~x]
+                          (printf "dbg %s:%s> %s is %s\n"
+                                  ~*ns*
+                                  ~(:line (meta &form))
+                                  ~(pr-str x)
+                                  (with-out-str
+                                    (clojure.pprint/pprint x#)))
+                          (flush)
+                          x#))]}}
