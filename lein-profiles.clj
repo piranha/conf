02838002b5c9de6379d0221b37597ef8ce97a5dc
Alexander Solovyov <alexander@solovyov.net>
2021-12-29 10:15:57 +0200

    lein ancient -> antq

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[lein-ancient "0.6.15"]
-                  [com.jakemccrary/lein-test-refresh "0.24.1"]]
-        :dependencies [[hashp "0.1.1"]]
-        :injections [(require 'hashp.core)]}}
+{:user {:plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]]
+        :aliases      {"outdated" ["run" "-m" "antq.core"]}
+        :dependencies [[com.github.liquidz/antq "RELEASE"]
+                       [hashp "0.1.1"]]
+        :injections   [(require 'hashp.core)]}}
